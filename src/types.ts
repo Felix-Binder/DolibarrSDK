87b636bd4421d762af53f7e8e692260c8122932e
Felix Binder <>
2022-04-18 22:49:49 +0200

    fix: create types & initial interface

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+export type DolibarrConfig = {
+    server: string,
+    uri?: string,
+    token: string | null,
+    transporter?: any,
+    authentication?: {
+        username: string,
+        password: string
+    }
+};
+
+export type DolibarrOperator = "agendaevents" | "bankaccounts" | "contacts" | "contracts" | "documents" | "invoices" | "login" | "projects" | "proposals" | "setup" | "status" | "supplierinvoice" | "supplierorder" | "tasks" | "thirdparties" | "tickets" | "users";
+
+export type DolibarrRequestOptions = {
+    socid?: string,
+    sortfield?: string,
+    sortorder?: "ASC" | "DESC",
+    limit?: number,
+    page?: string,
+    sqlfilters?: string
+}
+
+export interface IOperator<C, U> {
+    listMany(options?:DolibarrRequestOptions):Promise<any>;
+    listOne(id: number):Promise<any>;
+    create(payload: C):Promise<any>;
+    update(id: number, payload: U):Promise<any>;
+    delete(id: number):Promise<any>;
+}
