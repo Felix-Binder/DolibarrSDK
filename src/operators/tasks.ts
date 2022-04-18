40a516578b041bd0111a10bb1cd29718ea255b9a
Felix Binder <>
2022-04-19 01:49:18 +0200

    fix: extend Operator

--- src/operators/tasks.ts
+++ src/operators/tasks.ts
@@ -1,41 +1,40 @@
 import { Transporter } from '../transporter';
 import { DolibarrRequestOptions } from '../types';
 import { ITasks, TasksPayload } from '../tasks';
+import { Operator } from '../operator';
 
-export class Tasks implements ITasks {
-    public _transporter: Transporter;
-
+export class Tasks extends Operator implements ITasks {
     constructor(transporter: Transporter) {
-        this._transporter = transporter;
+        super(transporter, "tasks")
     }
 
     async listMany(options?: DolibarrRequestOptions) {
-        return await this._transporter.get("tasks", "/", {
+        return await this._transporter.get(this._operator, "/", {
             params: options
         });
     }
 
-    async listOne(id?:number) {
-        return await this._transporter.get("tasks", `/${id}`);
+    async listOne(id: number) {
+        return await this._transporter.get(this._operator, `/${id}`);
     }
     
-    async create(payload:any) {
-
+    async create(payload: TasksPayload["create"]) {
+        return await this._transporter.post(this._operator, "/", payload);
     }
 
-    async delete(id:number) {
-
+    async update(id: number, payload: TasksPayload["update"]) {
+        return await this._transporter.put(this._operator, `/${id}`, payload);
     }
-
-    async update(id:number, payload:any) {
-
+    
+    async delete(id: number) {
+        return await this._transporter.delete(this._operator, `/${id}`);
     }
 
     async addTimeSpent(id:number, payload: TasksPayload["addTimeSpent"]) {
-
+        return await this._transporter.post(this._operator, `/${id}/addtimespent`, payload);
     }
 
     async roles(id:number) {
-
+        return await this._transporter.get(this._operator, `/${id}/roles`);
     }
 }
