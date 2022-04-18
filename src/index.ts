35b85a7679bad50905977a5a7ef516f32861c3a8
Felix Binder <>
2022-04-18 22:52:40 +0200

    fix: export base and interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-export default function add(a:number, b:number):number {
-  return a + b;
-}
+// base
+export * from './dolibarr';
+export * from './types';
+
+// interfaces
+export * from './status';
+export * from './tasks';
+export * from './tickets';
