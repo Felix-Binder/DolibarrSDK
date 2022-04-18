517db5523aa27e12ce27fc935e9592a7ce2193ae
Felix Binder <>
2022-04-18 23:43:19 +0200

    feat: extend of operator class

--- src/operators/tickets.ts
+++ src/operators/tickets.ts
@@ -1,37 +1,36 @@
 import { Transporter } from '../transporter';
 import { DolibarrRequestOptions } from '../types';
 import { ITickets, TicketPayload } from '../tickets';
+import { Operator } from '../operator';
 
-export class Tickets implements ITickets {
-    private _transporter: Transporter;
-
+export class Tickets extends Operator implements ITickets {
     constructor(transporter: Transporter) {
-        this._transporter = transporter;
+        super(transporter, "tickets")
     }
 
     async listMany(options?: DolibarrRequestOptions) {
-        return await this._transporter.get("tickets", "/", {
+        return await this._transporter.get(this._operator, "/", {
             params: options
         });
     }
     
     async listOne(id:number) {
-        return await this._transporter.get("tickets", `/${id}`);
+        return await this._transporter.get(this._operator, `/${id}`);
     }
 
     async create(payload: TicketPayload["create"]) {
-        return await this._transporter.post("tickets", "/", payload);
+        return await this._transporter.post(this._operator, "/", payload);
     }
 
     async update(id:number, payload:Record<string, any>) {
-        return await this._transporter.put("tickets", `/${id}`, payload);
+        return await this._transporter.put(this._operator, `/${id}`, payload);
     }
 
     async delete(id:number) {
-        return await this._transporter.delete("tickets", `/${id}`);
+        return await this._transporter.delete(this._operator, `/${id}`);
     }
 
     async newMessage(payload: TicketPayload["newMessage"]) {
-        return await this._transporter.post("tickets", "/newmessage", payload);
+        return await this._transporter.post(this._operator, "/newmessage", payload);
     }
 }
