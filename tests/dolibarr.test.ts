561c1d79fd4454dacd99398f91c6a066bf494529
Felix-Binder <35593162+Felix-Binder@users.noreply.github.com>
2022-04-19 21:56:06 +0200

    fix update dolibarr tests

--- tests/dolibarr.test.ts
+++ tests/dolibarr.test.ts
@@ -1,16 +1,16 @@
 import 'dotenv/config';
 import { Dolibarr } from '../src';
+import { expect } from 'chai';
 
-const main = async () => {
+describe("Test Config", ():void => {
     const dolibarr = new Dolibarr({
-        server: process.env.DOLIBARR_SERVER||"",
-        token: process.env.DOLIBARR_TOKEN||""
-    })
-    await dolibarr.items.users.info().then(response => {
-        console.log("Response: ", response.data);
-    }).catch(error => {
-        console.error("Error: ", error);
+        server: process.env.DOLIBARR_SERVER,
+        token: process.env.DOLIBARR_TOKEN
     });
-}
-
-main();
+    it("Config URL", (done):void => {
+        const url = dolibarr.url;
+        console.log(url)
+        expect(url).to.be.a("string");
+        done()
+    });
+});
