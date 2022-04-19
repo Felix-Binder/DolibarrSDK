ce427522527ffdec4d94bbd35c18b50dd10c6dbd
Felix-Binder <35593162+Felix-Binder@users.noreply.github.com>
2022-04-19 20:19:08 +0200

    fix: publish config & eslint config

--- tests/dolibarr.test.ts
+++ tests/dolibarr.test.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config';
-import { Dolibarr } from '../src/index';
+import { Dolibarr } from '../src';
 
 const main = async () => {
     const dolibarr = new Dolibarr({
         server: process.env.DOLIBARR_SERVER||"",
         token: process.env.DOLIBARR_TOKEN||""
     })
-    await dolibarr.users.info().then(response => {
+    await dolibarr.items.users.info().then(response => {
         console.log("Response: ", response.data);
     }).catch(error => {
         console.error("Error: ", error);
