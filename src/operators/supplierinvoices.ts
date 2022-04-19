68d9700c88ad0477a6e251a7c0869182c13436f8
Felix Binder <>
2022-04-19 16:21:30 +0200

    feat: create supplierinvoices operator

---
+++ src/operators/supplierinvoices.ts
@@ -0,0 +1,62 @@
+import { Transporter } from '../transporter';
+import { DolibarrRequestOptions, TypeMap } from '../types';
+import { ISupplierinvoices, Payload } from '../supplierinvoices';
+import { Operator } from '../operator';
+
+export class Supplierinvoices extends Operator implements ISupplierinvoices {
+    constructor(transporter: Transporter) {
+        super(transporter, "supplierinvoices")
+    }
+
+    async listMany(options?: DolibarrRequestOptions) {
+        return await this._transporter.get(this._operator, "/", {
+            params: options
+        });
+    }
+    
+    async listOne(id: number) {
+        return await this._transporter.get(this._operator, `/${id}`);
+    }
+
+    async create(payload: Payload["create"]) {
+        return await this._transporter.post(this._operator, "/", payload);
+    }
+
+    async update(id: number, payload: Payload["update"]) {
+        return await this._transporter.put(this._operator, `/${id}`, payload);
+    }
+
+    async delete(id: number) {
+        return await this._transporter.delete(this._operator, `/${id}`);
+    }
+
+    // Additionals
+
+    async getLines(id: number) {
+        return await this._transporter.get(this._operator, `/${id}/lines`);
+    }
+
+    async addLine(id: number, payload: Payload["addLine"]): Promise<any> {
+        return await this._transporter.post(this._operator, `/${id}/lines`, payload);
+    }
+
+    async delLine(id: number, line: number) {
+        return await this._transporter.delete(this._operator, `/${id}/lines/${line}`);
+    }
+
+    async putLine(id: number, line: number, payload: Payload["putLine"]) {
+        return await this._transporter.put(this._operator, `/${id}/lines/${line}`, payload);
+    }
+
+    async getPayments(id: number) {
+        return await this._transporter.get(this._operator, `/${id}/payments`);
+    }
+
+    async addPayment(id: number, payload: Payload["addPayments"]) {
+        return await this._transporter.post(this._operator, `/${id}/payments`, payload);
+    }
+
+    async validate(id: number, payload: Payload["validate"]) {
+        return await this._transporter.post(this._operator, `/${id}/validate`, payload);
+    }
+}
--- src/supplierinvoices.ts
+++ src/supplierinvoices.ts
@@ -1,18 +1,63 @@
 import { IOperator, TypeMap } from './types';
 
-export type SupplierinvoicesPayload = {
+export type Payload = {
     create: TypeMap,
     update: TypeMap,
     addLine: TypeMap,
     putLine: TypeMap,
     addPayments: TypeMap,
+    validate: {
+        idwarehouse?: number,
+        notrigger?: 0 | 1
+    }
 }
 
-export interface ISupplierinvoices extends IOperator<SupplierinvoicesPayload["create"], SupplierinvoicesPayload["update"]> {
-    getLines(supplierinvoices: number):Promise<any>;
-    addLine(supplierinvoices: number, payload: SupplierinvoicesPayload["addLine"]):Promise<any>;
-    delLine(supplierinvoices: number, line: number):Promise<any>;
-    putLine(supplierinvoices: number, line: number, payload: SupplierinvoicesPayload["putLine"]):Promise<any>;
-    addPayments(id: number, payload: SupplierinvoicesPayload["addPayments"]):Promise<any>;
-    validate(id: number):Promise<any>;
+export interface ISupplierinvoices extends IOperator<Payload["create"], Payload["update"]> {
+    /**
+     * Get lines of a supplier invoice
+     * @param {number} id ID of supplier invoice
+    **/
+    getLines(id: number):Promise<any>;
+
+    /**
+     * Add a line to given supplier invoice
+     * @param {number} id ID of supplier invoice
+     * @param payload Request body payload
+    **/
+    addLine(id: number, payload: Payload["addLine"]):Promise<any>;
+
+    /**
+     * Deletes a line of a given supplier invoice
+     * @param {number} id ID of supplier invoice
+     * @param {number} line ID of the line to delete
+    **/
+    delLine(id: number, line: number):Promise<any>;
+
+    /**
+     * Update a line to a given supplier invoice 
+     * @param {number} id ID of supplier invoice
+     * @param {number} line ID of the line to update
+     * @param payload Request body payload
+    **/
+    putLine(id: number, line: number, payload: Payload["putLine"]):Promise<any>;
+
+    /**
+     * Get list of payments of a given supplier invoice 
+     * @param {number} id ID of supplier invoice
+    **/
+    getPayments(id: number):Promise<any>;
+
+    /**
+     * Add payment line to a specific supplier invoice with the remain to pay as amount
+     * @param {number} id ID of supplier invoice
+     * @param payload Request body payload
+    **/
+    addPayment(id: number, payload: Payload["addPayments"]):Promise<any>;
+
+    /**
+     * Validate an invoice
+     * @param {number} id ID of supplier invoice
+     * @param payload Request body payload
+    **/
+    validate(id: number, payload: Payload["validate"]):Promise<any>;
 }
